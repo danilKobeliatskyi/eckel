4fbaf2ce0af82a9e7accd49846fbe5ebcaec6274
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2017-12-07 15:15:51 +0200

    done exercise 3 and 4 from chapter 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,19 @@ package com.company;
 
 public class Main {
 
+    static final String S = "World";
+
     public static void main(String[] args) {
-	// write your code here
+	Test t = new Test();
+	Test t2 = new Test(S);
+    }
+}
+
+class Test {
+    Test (){
+        System.out.println("Hello");
+    }
+    Test (String S){
+        System.out.println("Hello" + S);
     }
 }
