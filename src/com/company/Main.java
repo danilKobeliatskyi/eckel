c716e61bc437517e7f2a347feb697dca8c5a0cf9
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2017-12-07 17:20:07 +0200

    done exercise 7 from chapter 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -29,6 +29,9 @@ public class Main {
 
         cookingMetod(2, 'l');
         cookingMetod('l', 2);
+
+        WitoutConstructor w = new WitoutConstructor();
+        w.text();
     }
 
     static class Test {
@@ -45,170 +48,87 @@ public class Main {
         void bark1(char x) {
             System.out.println("bark(char)");
         }
-
-        ;
-
         void bark1(byte x) {
             System.out.println("bark(byte)");
         }
-
-        ;
-
         void bark1(short x) {
             System.out.println("bark(short)");
         }
-
-        ;
-
         void bark1(int x) {
             System.out.println("bark(int)");
         }
-
-        ;
-
         void bark1(long x) {
             System.out.println("bark(long)");
         }
-
-        ;
-
         void bark1(float x) {
             System.out.println("bark(float)");
         }
-
-        ;
-
         void bark1(double x) {
             System.out.println("bark(double)");
         }
-
-        ;
-
         void bark2(byte x) {
             System.out.println("bark(byte)");
         }
-
-        ;
-
         void bark2(short x) {
             System.out.println("bark(short)");
         }
-
-        ;
-
         void bark2(int x) {
             System.out.println("bark(int)");
         }
-
-        ;
-
         void bark2(long x) {
             System.out.println("bark(long)");
         }
-
-        ;
-
         void bark2(float x) {
             System.out.println("bark(float)");
         }
-
-        ;
-
         void bark2(double x) {
             System.out.println("bark(double)");
         }
-
-        ;
-
         void bark3(short x) {
             System.out.println("bark(short)");
         }
-
-        ;
-
         void bark3(int x) {
             System.out.println("bark(int)");
         }
-
-        ;
-
         void bark3(long x) {
             System.out.println("bark(long)");
         }
-
-        ;
-
         void bark3(float x) {
             System.out.println("bark(float)");
         }
-
-        ;
-
         void bark3(double x) {
             System.out.println("bark(double)");
         }
-
-        ;
-
         void bark4(int x) {
             System.out.println("bark(int)");
         }
-
-        ;
-
         void bark4(long x) {
             System.out.println("bark(long)");
         }
-
-        ;
-
         void bark4(float x) {
             System.out.println("bark(float)");
         }
-
-        ;
-
         void bark4(double x) {
             System.out.println("bark(double)");
         }
-
-        ;
-
         void bark5(long x) {
             System.out.println("bark(long)");
         }
-
-        ;
-
         void bark5(float x) {
             System.out.println("bark(float)");
         }
-
-        ;
-
         void bark5(double x) {
             System.out.println("bark(double)");
         }
-
-        ;
-
         void bark6(float x) {
             System.out.println("bark(float)");
         }
-
-        ;
-
         void bark6(double x) {
             System.out.println("bark(double)");
         }
-
-        ;
-
         void bark7(double x) {
             System.out.println("bark(double)");
         }
-
-        ;
 
         void TestConst() {
             System.out.println("5 times: ");
@@ -311,6 +231,11 @@ public class Main {
             bark6(x);
             bark7(x);
             System.out.println();
+        }
+    }
+    static class WitoutConstructor{
+        public static void text(){
+            System.out.println("without constructor");
         }
     }
 }
