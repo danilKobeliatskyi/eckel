ac0a24ce114f06e8684e02a27998bac6827ea877
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2017-12-10 17:25:19 +0200

    done exercise 8 from chapter 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,25 @@ public class Main {
 
     static final String S = "World";
 
-    public static void cookingMetod(int i, char l) {
-        System.out.println("woter: " + i + ", liters:" + 'l');
-    }
+    public static void cookingMetod(int i, char l) {System.out.println("woter: " + i + ", liters:" + 'l');}
+    public static void cookingMetod(char l, int i) {System.out.println("liters:" + 'l'+ ", woter" + i);}
 
-    public static void cookingMetod(char l, int i) {
-        System.out.println("liters:" + 'l'+ ", woter" + i);
-    }
+        Main Temp(){
+            System.out.println("Good");
+            return this;
+        }
+        void result() {
+            Main s = new Main();
+            s.Temp();
+            this.Temp();
+        }
 
     public static void main(String[] args) {
         Test t = new Test();
         Test t2 = new Test(S);
+
+        Main text =new Main();
+        text.result();
 
         Dog d = new Dog();
         d.TestConst();
@@ -25,7 +33,6 @@ public class Main {
         d.TestLong();
         d.TestFloat();
         d.TestDouble();
-
 
         cookingMetod(2, 'l');
         cookingMetod('l', 2);
