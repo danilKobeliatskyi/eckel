d6328b8981b28bdcf7c648909b1aace7947a69ec
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-19 23:40:46 +0200

    done exercise 15 from chapter 9

--- src/com/chapter9/Car.java
+++ src/com/chapter9/Car.java
@@ -1,9 +1,32 @@
 package com.chapter9;
 
-public class Car {
+public abstract class Car implements Vehicle{
+    public void open(){
+        System.out.println("Open left dore");
+    }
 
-    static void e(Engine x){x.start();}
-    static void w(Wills x){x.turn();}
-    static void d(Dore x){x.close();}
-    static void v(Vehicle x){x.velocity();}
+    public void close(){
+        System.out.println("Close left dore");
+    }
+
+    public void start(){
+        System.out.println("Start engine");
+    }
+
+    public int quantity(){
+        int wills = 4;
+        return wills;
+    }
+
+    public void turn(){
+        System.out.println("turn all wills");
+    }
+
+    public void velocity(){
+        System.out.println("Velocity 210 km");
+    }
+
+    public void finish(){
+        System.out.println("Stop engine");
+    }
 }
--- src/com/chapter9/SpeedCar.java
+++ src/com/chapter9/SpeedCar.java
@@ -1,33 +1,9 @@
 package com.chapter9;
 
-public class SpeedCar extends Object implements Vehicle {
+public class SpeedCar extends Car {
 
-    public void open(){
-        System.out.println("Open left dore");
-    }
+    SpeedCar(){
 
-    public void close(){
-        System.out.println("Close left dore");
-    }
-
-    public void start(){
-        System.out.println("Start engine");
-    }
-
-    public int quantity(){
-        int wills = 4;
-        return wills;
-    }
-
-    public void turn(){
-        System.out.println("turn all wills");
-    }
-
-    public void velocity(){
-        System.out.println("Velocity 210 km");
-    }
-
-    public void finish(){
-        System.out.println("Stop engine");
+        System.out.println("SpeedCar");
     }
 }
