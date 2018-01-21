3904f6c9bd53a71f436c0f2a8b37ae598cc1454e
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-21 15:23:54 +0200

    First program from curse

---
+++ src/com/javacurse/SalaryCalculator.java
@@ -0,0 +1,20 @@
+package com.javacurse;
+
+public class SalaryCalculator {
+
+    public static void main(String[] args){
+
+        String career;
+        System.out.println("Program a starting");
+        career = "Software Developer";
+        System.out.println("My career is: " + career);
+
+        int hoursPerWeek = 40;
+        int weeksPerYear = 50;
+        double rate = 42.50;
+        career = "Web developer"
+
+        double salary = hoursPerWeek * weeksPerYear * rate;
+        System.out.println("My salary is a: " + career + " at the rate of $ " + rate + " per hour is $" + salary + " per year");
+    }
+}
