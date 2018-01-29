f39a03ae6490e66b667999817cd05815cf4dee4e
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-29 16:06:19 +0200

    done exercise 2 from chapter 11

--- src/com/chapter11/Main.java
+++ src/com/chapter11/Main.java
@@ -12,6 +12,8 @@ public class Main {
         gerbilList.add(new Gerbil());
         gerbilList.add(new Gerbil());
         gerbilList.add(new Gerbil());
+        for (int i = 0; i < gerbilList.size(); i++)
+            System.out.println(gerbilList.get(i));
         for (Gerbil gerbil : gerbilList)
             Gerbil.hop();
     }
