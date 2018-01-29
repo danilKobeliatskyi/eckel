9b35d222f84f3ce01441df2b0225309318e7320d
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-29 17:20:12 +0200

    done exercise 4 from chapter 11

--- src/com/chapter11/Main.java
+++ src/com/chapter11/Main.java
@@ -1,7 +1,6 @@
 package com.chapter11;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
@@ -16,5 +15,16 @@ public class Main {
             System.out.println(gerbilList.get(i));
         for (Gerbil gerbil : gerbilList)
             Gerbil.hop();
+
+        Generator gen = new Generator();
+        String[] a = new String[10];
+        gen.fill1(a);
+        for(String s : a) System.out.print(s + ", ");
+        System.out.println();
+        System.out.println(gen.fill(new ArrayList<String>(), 10));
+        System.out.println(gen.fill(new LinkedList<String>(), 10));
+        System.out.println(gen.fill(new HashSet<String>(), 10));
+        System.out.println(gen.fill(new LinkedHashSet<String>(), 10));
+        System.out.println(gen.fill(new TreeSet<String>(), 10));
     }
 }
