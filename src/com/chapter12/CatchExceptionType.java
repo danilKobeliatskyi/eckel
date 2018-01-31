5cf1bee992933bf97a074e210ec466e23f65c426
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-31 16:14:52 +0200

    Done exercise 13 from chapter 12

--- src/com/chapter12/CatchExceptionType.java
+++ src/com/chapter12/CatchExceptionType.java
@@ -1,6 +1,7 @@
 package com.chapter12;
 
 public class CatchExceptionType {
+    private static Integer[] x = new Integer[1];
     public static void f(int x) throws ExceptionTypeA, ExceptionTypeB, ExceptionTypeC {
         if(x < 0) throw new ExceptionTypeA("x < 0");
         if(x == 0) throw new ExceptionTypeB("x == 0");
@@ -8,12 +9,15 @@ public class CatchExceptionType {
     }
     public CatchExceptionType() {
         try {
+            f(x[0]);
             f(0);
             f(1);
             f(-1);
         } catch(Exception e) {
             System.out.println("Caught Exception");
             e.printStackTrace(System.out);
+        } finally {
+            System.out.println("Finally: do something");
         }
     }
 }
