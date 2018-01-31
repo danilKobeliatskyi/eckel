ba4c0ac9e173930ef9df86bc1672029df9e1bf6c
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-31 17:09:59 +0200

    Done exercise 17 from chapter 12

---
+++ src/com/chapter12/Description.java
@@ -0,0 +1,12 @@
+package com.chapter12;
+
+public class Description {
+    private String s;
+    Description(String s) {
+        this.s = s;
+        System.out.println("Creating Description " + s);
+    }
+    protected void dispose() {
+        System.out.println("disposing Description " + s);
+    }
+}
