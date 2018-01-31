d555cb955c44f1fdcf0297a5ca33bd02be058799
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-31 20:04:51 +0200

    Done exercise 30 from chapter 12

---
+++ src/com/chapter12/WrapCheckedExceptions.java
@@ -0,0 +1,19 @@
+package com.chapter12;
+
+public class WrapCheckedExceptions {
+    void throwRuntimeException(int type) {
+        try {
+            switch(type) {
+                case(0):
+                    throw new Annoyance();
+                case(1):
+                    throw new Sneeze();
+                case(2):
+                    throw new RuntimeException("Where am I?");
+                default: return;
+            }
+        } 	catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
