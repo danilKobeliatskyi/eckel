262bb8b82e2491f59393ae82523dc3f9b879b20d
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-31 19:55:19 +0200

    Done exercise 28 from chapter 12

--- src/com/chapter12/MyException.java
+++ src/com/chapter12/MyException.java
@@ -1,6 +1,6 @@
 package com.chapter12;
 
-public class MyException extends Exception {
+public class MyException extends RuntimeException {
     public MyException(String msg) {
         super(msg);
         System.out.println("MyException(String msg)");
--- src/com/chapter12/Treatment.java
+++ src/com/chapter12/Treatment.java
@@ -5,14 +5,8 @@ public class Treatment {
         System.out.println("Throwing MyException from f()");
         throw new MyException("From f()");
     }
+
     public static void main(String[] args) {
-        try {
-            f();
-        } catch(MyException e) {
-            System.err.println("Caught MyException");
-            e.printStackTrace();
-        } finally {
-            System.out.println("Made it to finally");
-        }
+        f();
     }
 }
