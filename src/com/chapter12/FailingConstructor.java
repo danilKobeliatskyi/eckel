bed56b05e79a1e8743e73c08322dee0228b425e4
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-31 19:06:33 +0200

    Done exercise 23 from chapter 12

--- src/com/chapter12/FailingConstructor.java
+++ src/com/chapter12/FailingConstructor.java
@@ -1,17 +1,39 @@
 package com.chapter12;
 
+import java.io.*;
+
 public class FailingConstructor {
-    Integer[] integers = new Integer[2];
-    String s;
-    FailingConstructor(String s) throws Exception {
-        integers[0] = 0;
-        integers[1] = 1;
-        integers[2] = 2;
-        this.s = s;
+    private Integer[] ia = new Integer[2];
+    private static Disposable d0;
+    private static Disposable d1;
+    FailingConstructor() throws Exception {
+        try {
+            d0 = new Disposable();
+            try {
+                ia[2] = 2;
+                try {
+                    d1 = new Disposable();
+                } catch(Exception e) {
+                    System.out.println("Caught e in inner try loop");
+                    e.printStackTrace(System.err);
+                    System.out.println("Failed to create d1");
+                }
+            } catch(Exception e) {
+                System.out.println("Caught e in middle try loop");
+                e.printStackTrace(System.err);
+                System.out.println("Disposing d0");
+                d0.dispose();
+            }
+        } catch(Exception e) {
+            System.out.println("Caught e in outer try loop");
+            e.printStackTrace(System.err);
+            System.out.println("Failed to create d0");
+        }
+
     }
     public static void main(String[] args) {
         try {
-            FailingConstructor failingConstructor = new FailingConstructor("hi");
+            FailingConstructor failingConstructor = new FailingConstructor();
         } catch(Exception e) {
             System.err.println("Caught Exception in main()");
             e.printStackTrace(System.err);
