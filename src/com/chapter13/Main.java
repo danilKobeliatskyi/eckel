409784d27ef4c21af5db5c16c7df30515db271ba
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-31 23:17:53 +0200

    Done exercise 1 from chapter 13

---
+++ src/com/chapter13/Main.java
@@ -0,0 +1,9 @@
+package com.chapter13;
+
+public class Main {
+    public static void main(String[] args){
+        SprinklerSystem sprinkler = new SprinklerSystem();
+        sprinkler.toString();
+        System.out.println(sprinkler);
+    }
+}
---
+++ src/com/chapter13/WaterSource.java
@@ -0,0 +1,14 @@
+package com.chapter13;
+
+public class WaterSource {
+    private String s;
+
+    WaterSource() {
+        System.out.println("WaterSource()");
+        s = "Constructed";
+    }
+
+    public String toString() {
+        return s;
+    }
+}
