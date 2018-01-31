89f32010f58aada2c470fd3d56148e547166cd0a
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-01-31 19:43:17 +0200

    Done exercise 26 from chapter 12

---
+++ src/com/chapter12/MainException.java
@@ -0,0 +1,10 @@
+package com.chapter12;
+
+import java.io.FileInputStream;
+
+public class MainException {
+    public static void main(String[] args) throws Exception {
+        FileInputStream file = new FileInputStream("null.jv");
+        file.close();
+    }
+}
