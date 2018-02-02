a05e3654e8b0078a3500355f0015bfe538b21ac2
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-02 13:54:41 +0200

    Done excercise 7 and 9 from chapter 13

---
+++ src/com/chapter13/Replacing.java
@@ -0,0 +1,8 @@
+package com.chapter13;
+
+public class Replacing {
+    public static String knights = "Then, when you have found the shrubbery, you must cut down the mightiest tree in the forest... with... a herring!";
+    public static void main(String[] args) {
+        System.out.println(knights.replaceAll("[AEIOUaeiou]", "_"));
+    }
+}
