45a02924c7912f724710410368b6720c9353ba26
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-04 18:57:23 +0200

    Done exercise 4 from chapter 22

---
+++ src/Danil/chapter19/Button1.java
@@ -0,0 +1,18 @@
+package Danil.chapter19;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static Danil.chapter19.SwingConsole.*;
+
+public class Button1 extends JFrame {
+    private JButton b1 = new JButton("Button1"), b2 = new JButton("Button2");
+    public Button1(){
+        setLayout(new FlowLayout());
+        add(b1);
+        add(b2);
+    }
+    public static void main(String[] args){
+        run(new Button1(), 200, 100);
+    }
+}
