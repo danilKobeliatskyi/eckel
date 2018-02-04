97461a719c787a75ed4f15106d09e9b0843828a8
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-04 17:50:51 +0200

    Done exercise 3 from chapter 22

--- src/Danil/chapter19/SubmitSwingProgram.java
+++ src/Danil/chapter19/SubmitSwingProgram.java
@@ -4,28 +4,19 @@ import javax.swing.*;
 import java.util.concurrent.TimeUnit;
 
 public class SubmitSwingProgram extends JFrame {
-    JLabel label;
-    public SubmitSwingProgram(){
-        super("Hello Swing");
-        label = new JLabel("A label");
-        add(label);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
-        setVisible(true);
-    }
     static SubmitSwingProgram ssp;
-    public static void main(String[] args) throws Exception{
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                ssp = new SubmitSwingProgram();
-            }
-        });
+
+    public static void main(String[] args) throws Exception {
+    SwingConsole console = new SwingConsole();
+    JFrame frame = new JFrame();
+        console.run(frame, 300, 300);
+        JLabel label = new JLabel("A label");
+        frame.add(label);
         TimeUnit.SECONDS.sleep(1);
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                ssp.label.setText("Hey! Hello!");
+                label.setText("Hey! Hello");
             }
         });
     }
---
+++ src/Danil/chapter19/SwingConsole.java
@@ -0,0 +1,17 @@
+package Danil.chapter19;
+
+import javax.swing.*;
+
+public class SwingConsole {
+    public static void run(final JFrame frame, final int width, final int height){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                frame.setTitle(frame.getClass().getSimpleName());
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(width, height);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
