65d679e00ec4293557a4d47c53f3ca9fd36aac63
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-12 15:09:37 +0200

    Done exercise 10 from chapter 14

---
+++ src/Danil/chapter14/WhatIsAnMassifChar.java
@@ -0,0 +1,11 @@
+package Danil.chapter14;
+
+public class WhatIsAnMassifChar {
+    public static void main(String[] args) {
+        char[] c = new char[10];
+        System.out.println("Superclass of char[] c: " +
+                c.getClass().getSuperclass());
+        System.out.println("char[] c instanceof Object: " +
+                (c instanceof Object));
+    }
+}
