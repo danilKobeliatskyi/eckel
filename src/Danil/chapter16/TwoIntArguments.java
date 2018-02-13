82e689362385e43c2bd4b30f19800390d5fab3d6
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-13 21:49:56 +0200

    Done exercise 7 from chapter 16

--- src/Danil/chapter16/TwoIntArguments.java
+++ src/Danil/chapter16/TwoIntArguments.java
@@ -3,17 +3,20 @@ package Danil.chapter16;
 import java.util.Arrays;
 
 public class TwoIntArguments {
-    public static BerylliumSphere[][] sphereAM(int size1, int size2) {
-        BerylliumSphere[][] result = new BerylliumSphere[size1][size2];
+    public static BerylliumSphere[][][] sphereAM(int size1, int size2, int size3) {
+        BerylliumSphere[][][] result = new BerylliumSphere[size1][size2][size3];
         for(int i = 0; i < size1; i++) {
-            result[i] = new BerylliumSphere[size2];
-            for(int j = 0; j < size2; j++)
-                result[i][j] = new BerylliumSphere();
+            result[i] = new BerylliumSphere[size2][size3];
+            for(int j = 0; j < size2; j++) {
+                result[i][j] = new BerylliumSphere[size3];
+                for(int k = 0; k < size3; k++)
+                    result[i][j][k] = new BerylliumSphere();
+            }
         }
         return result;
     }
     public static void main(String[] args) {
-        System.out.println(Arrays.deepToString(sphereAM(2,2)));
-        System.out.println(Arrays.deepToString(sphereAM(4,3)));
+        System.out.println(Arrays.deepToString(sphereAM(2,2,2)));
+        System.out.println(Arrays.deepToString(sphereAM(4,3,2)));
     }
 }
