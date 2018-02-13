a0b76c9ba55a8e9b0c15e354193084b12a5c6b6f
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-14 00:52:01 +0200

    Done exercise 8 from chapter 16

---
+++ src/Danil/chapter16/TMassiv.java
@@ -0,0 +1,18 @@
+package Danil.chapter16;
+
+public class TMassiv<T> {
+    @SuppressWarnings("unchecked")
+    T[] a = (T[])new Object[2];
+    public static void main(String[] args) {
+        String[] sa = new String[2];
+        // sa[0] = new Integer(0); error: incompatible types
+        Object[] oa = new Object[3];
+        oa[0] = new String("hi");
+        oa[1] = new A();
+        int x = 2;
+        oa[2] = x;
+        System.out.println("oa[2]: " + oa[2].getClass());
+        oa = sa;
+        System.out.println("oa: " + oa.getClass());
+    }
+}
