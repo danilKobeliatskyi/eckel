2da165304fcf2054f3330cf0e67fbc8a660f42fb
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-13 21:40:38 +0200

    Done exercise 5 from chapter 16

---
+++ src/Danil/chapter16/MultidimensionalArray.java
@@ -0,0 +1,10 @@
+package Danil.chapter16;
+
+import java.util.Arrays;
+
+public class MultidimensionalArray {
+    public static void main(String[] args) {
+        A[][][] a3 = new A[3][3][3];
+        System.out.println(Arrays.deepToString(a3));
+    }
+}
