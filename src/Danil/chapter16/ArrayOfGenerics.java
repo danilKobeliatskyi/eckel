d63699026db43c5658baf732cc7d3f56e655837b
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-14 11:09:51 +0200

    Done exercise 10 from chapter 16

---
+++ src/Danil/chapter16/ArrayOfGenerics.java
@@ -0,0 +1,23 @@
+package Danil.chapter16;
+import java.util.*;
+
+public class ArrayOfGenerics {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args){
+        List<List<String>> lls = new ArrayList<List<String>>();
+        List<List> lists = new ArrayList<List>();
+        lls.add(new ArrayList<String>());
+
+        List<Object> objectList = new ArrayList<Object>();
+        objectList.add(lls);
+
+        List<List<BerylliumSphere>> listBery = new ArrayList<List<BerylliumSphere>>();
+        for (int i = 0; i < 5; i++){
+            listBery.add(new ArrayList<BerylliumSphere>());
+            for(int j = 0; j < 3; j++) {
+                listBery.get(i).add(new BerylliumSphere());
+            }
+        }
+        System.out.println(listBery);
+    }
+}
