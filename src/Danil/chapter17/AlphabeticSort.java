8fa8745c822cb19fcbd2a0e18f2d46e54d39b049
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-20 20:52:57 +0200

    Done exercise 9 from chapter 17

---
+++ src/Danil/chapter17/AlphabeticSort.java
@@ -0,0 +1,14 @@
+package Danil.chapter17;
+
+import java.util.TreeSet;
+
+public class AlphabeticSort {
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+        RandomGenerator.String rgs = new RandomGenerator.String(5);
+        for(int i = 0; i < 20; i++) {
+            ts.add(rgs.next());
+        }
+        System.out.print(ts);
+    }
+}
---
+++ src/Danil/chapter17/Generator.java
@@ -0,0 +1,3 @@
+package Danil.chapter17;
+
+public interface Generator<T> { T next(); }
