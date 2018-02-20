67a371c116f37143004e0bbf55aa1ab94ece4538
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-20 12:07:12 +0200

    Done exercise 2 from chapter 17

---
+++ src/Danil/chapter17/CountriesThatBeginWithA.java
@@ -0,0 +1,20 @@
+package Danil.chapter17;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class CountriesThatBeginWithA {
+    public static void main(String[] args) {
+        Map<String,String> hm = new HashMap<String,String>();
+        Set<String> hs = hm.keySet();
+        Pattern p = Pattern.compile("A[a-zA-Z]*");
+        for(int i = 0; i < Countries.DATA.length; i++) {
+            if(p.matcher(Countries.DATA[i][0]).matches())
+                hm.put(Countries.DATA[i][0], Countries.DATA[i][1]);
+        }
+        System.out.println(hm);
+        System.out.println(hs);
+    }
+}
