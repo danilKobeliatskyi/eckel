ac5e60ae590a43d6d5bf8516f30d943008103ea2
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-20 23:37:22 +0200

    Done exercise 13 from chapter 17

---
+++ src/Danil/chapter17/WordCounter.java
@@ -0,0 +1,25 @@
+package Danil.chapter17;
+
+import Danil.chapter13.TextFile;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class WordCounter {
+    public static void main(String[] args) {
+        String fileName = "C:\\Users\\Danil\\IdeaProjects\\eckel\\src\\Danil\\chapter17\\WordCounter.java";
+        Set<String> words = new TreeSet<String>(new TextFile(fileName, "\\W+"));
+        AssociativeArray<String,Integer> wordCount = new AssociativeArray<String,Integer>(words.size());
+        ArrayList<String> fileList = new TextFile(fileName, "\\W+");
+        
+        for(String s : words) {
+            int count = 0;
+            for(String t : fileList) {
+                if(t.equals(s)) count++;
+            }
+            wordCount.put(s, count);
+        }
+        System.out.print(wordCount);
+    }
+}
