56fda815090ae8759d11066d6e7297e711fc05ee
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-21 19:23:46 +0200

    Done exercise 10 from chapter 18

--- src/danil/chapter18/ReverseLines.java
+++ src/danil/chapter18/ReverseLines.java
@@ -4,23 +4,29 @@ import java.util.*;
 
 
 public class ReverseLines {
-    public static String readAndReverse(String filename) throws IOException {
-        BufferedReader in = new BufferedReader(new FileReader(filename));
+    public static String readReverseFind(String[] sa) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(sa[0]));
         String s;
-        LinkedList<String> list = new LinkedList<String>();
+        List<String> find = new ArrayList<>();
+        for(String t : sa) find.add(t);
+        find.remove(0);
+        LinkedList<String> list = new LinkedList<>();
         StringBuilder sb = new StringBuilder();
-        while((s = in.readLine()) != null)
-            list.add(s.toUpperCase());
+        while((s = in.readLine()) != null) {
+            List<String> line = Arrays.asList(s.split("\\W+"));
+            if(!(Collections.disjoint(line, find)))
+                list.add(s);
+        }
         while(list.peekLast() != null)
             sb.append(list.pollLast() + "\n");
         in.close();
         return sb.toString();
     }
     public static void main(String[] args) throws IOException {
-        if(args.length != 1) {
-            System.out.println("Usage: enter file name");
+        if(args.length < 2) {
+            System.out.println("Usage: enter file name followed by words to find in lines of that file");
             System.exit(1);
         }
-        System.out.print(readAndReverse(args[0]));
+        System.out.print(readReverseFind(args));
     }
 }
