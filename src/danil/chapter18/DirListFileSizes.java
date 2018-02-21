60a0d0b80fed33d276bc62ddf9b90cf4b5211afe
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-21 15:56:11 +0200

    Done exercise 3 from chapter 18

---
+++ src/danil/chapter18/DirListFileSizes.java
@@ -0,0 +1,29 @@
+package danil.chapter18;
+import java.io.*;
+import java.util.regex.*;
+import java.util.*;
+
+public class DirListFileSizes {
+    public static void main(final String[] args) {
+        File path = new File(".");
+        File[] files;
+        if(args.length == 0)
+            files = path.listFiles();
+        else
+            files = path.listFiles(new FileFilter() {
+                private Pattern pattern = Pattern.compile(args[0]);
+                public boolean accept(File file) {
+                    return pattern.matcher(file.getPath()).matches();
+                }
+            });
+
+        Arrays.sort(files);
+        long sum = 0;
+        for(File f : files) {
+            System.out.print(f + ": ");
+            System.out.println(f.length());
+            sum += f.length();
+        }
+        System.out.println("Total size: " + sum);
+    }
+}
