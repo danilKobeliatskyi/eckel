bcb7d6ca48297dc53c461d3214e3f59c75f4ffde
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-02-21 18:43:59 +0200

    Done exercise 8 from chapter 18

--- src/danil/chapter18/ReverseLines.java
+++ src/danil/chapter18/ReverseLines.java
@@ -7,16 +7,21 @@ public class ReverseLines {
     public static String readAndReverse(String filename) throws IOException {
         BufferedReader in = new BufferedReader(new FileReader(filename));
         String s;
-        LinkedList<String> list = new LinkedList<>();
+        LinkedList<String> list = new LinkedList<String>();
         StringBuilder sb = new StringBuilder();
-        while((s = in.readLine()) != null)
+        while ((s = in.readLine()) != null)
             list.add(s);
-        while(list.peekLast() != null)
+        while (list.peekLast() != null)
             sb.append(list.pollLast() + "\n");
         in.close();
         return sb.toString();
     }
+
     public static void main(String[] args) throws IOException {
-        System.out.print(readAndReverse("C:\\Users\\Danil\\IdeaProjects\\eckel\\src\\danil\\chapter18\\ReverseLines.java"));
+        if (args.length != 1) {
+            System.out.println("Usage: enter file name");
+            System.exit(1);
+        }
+        System.out.print(readAndReverse(args[0]));
     }
 }
